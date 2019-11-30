b964fd36c9d51aab0801ef48041927e9e5eaa6e9
Atchayasekar <atchaya051997@gmail.com>
2019-12-01 01:03:32 +0530

    enable Edit Mode and View Mode

---
+++ app/src/main/java/com/atchaya/notes/LinedEditText.java
@@ -0,0 +1,46 @@
+package com.atchaya.notes;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.appcompat.widget.AppCompatEditText;
+
+
+
+public class LinedEditText extends AppCompatEditText{
+    private Rect mRect;
+    private Paint mPaint;
+
+
+    public LinedEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mRect = new Rect();
+        mPaint = new Paint();
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(2);
+        mPaint.setColor(0xFFFFD966);
+
+    } @Override
+    protected void onDraw(Canvas canvas) {
+        int height = ((View)this.getParent()).getHeight();
+        int lineheight = getLineHeight();
+        int numberofLines = height/lineheight;
+
+        Rect r = mRect;
+        Paint paint = mPaint;
+        int baseline = getLineBounds(0,r);
+        for (int i=0; i< numberofLines; i++){
+            canvas.drawLine(r.left, baseline + 1, r.right,  baseline + 1, paint);
+            baseline += lineheight;
+
+        }
+
+
+        super.onDraw(canvas);
+    }
+}
--- app/src/main/java/com/atchaya/notes/NoteActivity.java
+++ app/src/main/java/com/atchaya/notes/NoteActivity.java
@@ -2,31 +2,211 @@ package com.atchaya.notes;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.atchaya.notes.models.Note;
 
-public class NoteActivity extends AppCompatActivity {
+public class NoteActivity extends AppCompatActivity implements
+        View.OnTouchListener,
+        GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener,
+        View.OnClickListener
+
+
+
+
+{
+
     private static final String TAG = "NoteActivity";
+    private static final int EDIT_MODE_ENABLED = 1;
+    private static final int EDIT_MODE_DISBALED = 0;
+
+    // ui components
+    private LinedEditText mLinedEditText;
+    private EditText mEditTitle;
+    private TextView mViewTitle;
+    private RelativeLayout mCheckContainer,mBackArrowContainer;
+    private ImageButton mCheck, mBackArrow;
+
+    //Vars
+    private boolean mIsNewNote;
+    private Note mInitialNote;
+    private GestureDetector mGestureDetector;
+    private int mMode;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+        mLinedEditText = findViewById(R.id.note_text);
+        mEditTitle = findViewById(R.id.note_edit_title);
+        mViewTitle = findViewById(R.id.note_text_title);
+        mCheckContainer = findViewById(R.id.check_container);
+        mBackArrowContainer = findViewById(R.id.back_arrow_container);
+        mCheck = findViewById(R.id.toolbar_check);
+        mBackArrow = findViewById(R.id.toolbar_check);
+
+        if (getIncomingIntent()){
+
+            // this is NOT a new note(EDIT MODE)
+            setNewNoteProperties();
+            enableEditMode();
+        }
+        else{
+            // this is NOT a new note
+            setNoteProperties();
+        }
+      
+        setListener();
+    }
 
 
-        if (getIntent().hasExtra("selected_note")){
-            Note note = getIntent().getParcelableExtra("selected_note");
-            Log.d(TAG, "onCreate: " + note.toString());
+    private void  setListener(){
+
+        mLinedEditText.setOnTouchListener(this);
+        mGestureDetector =new GestureDetector( this, this);
+        mViewTitle.setOnClickListener(this);
+        mCheck.setOnClickListener(this);
+    }
+
+    private boolean getIncomingIntent() {
+        if (getIntent().hasExtra("selected_note")) {
+            mInitialNote = getIntent().getParcelableExtra("selected_note");
+            Log.d(TAG, "getIncomingIntent: " + mInitialNote.toString());
+
+            mMode = EDIT_MODE_DISBALED;
+            mIsNewNote = false;
+            return false;
+
+        }
+        mMode = EDIT_MODE_ENABLED;
+        mIsNewNote = true;
+        return true;
+    }
+
+        private void enableEditMode(){
+        mBackArrowContainer.setVisibility(View.GONE);
+        mCheckContainer.setVisibility(View.VISIBLE);
+
+        mViewTitle.setVisibility(View.GONE);
+        mEditTitle.setVisibility(View.VISIBLE);
+
+        mMode = EDIT_MODE_ENABLED;
+        }
+        private void disableEditMode(){
+        mBackArrowContainer.setVisibility(View.VISIBLE);
+        mCheckContainer.setVisibility(View.GONE);
+
+        mMode = EDIT_MODE_DISBALED;
         }
 
-        //VIEW
-        //EDIT
+    private void setNoteProperties(){
+        mViewTitle.setText(mInitialNote.getTitle());
+        mEditTitle.setText(mInitialNote.getTitle());
+        mLinedEditText.setText(mInitialNote.getContent());
 
 
     }
 
 
+    private void setNewNoteProperties(){
+        mViewTitle.setText("Note Title");
+        mEditTitle.setText("Note Title");
+    }
 
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        return mGestureDetector.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        Log.d(TAG, "onDoubleTap: double tapped!");
+        enableEditMode();
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (View.getId()){
+
+            case R.id.toolbar_check:{
+                disableEditMode();
+                break;
+            }
+            case  R.id.note_text_title:{
+                enableEditMode();
+                mEditTitle.requestFocus();
+                mEditTitle.setSelection(mEditTitle.length());
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mMode == EDIT_MODE_ENABLED){
+            OnClick(mCheck);
+        }
+        else {
+            super.onBackPressed();
+
+        }
+    }
 }
+
+
+
